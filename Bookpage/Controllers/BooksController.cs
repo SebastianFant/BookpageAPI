dbcac572f1bd0dda37c4282bb502b16e36e2c6cf
Sebastian fant <sebastianfant@hotmail.com>
2024-01-18 15:34:54 +0100

    Initital commit with controllers and jwt in place

--- Bookpage/Controllers/BooksController.cs
+++ Bookpage/Controllers/BooksController.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Bookpage.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Bookpage.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class BooksController : ControllerBase
